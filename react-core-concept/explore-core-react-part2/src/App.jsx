d66bd7ebcd977e3be723870baf2a13474b073fe7
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2026-01-13 00:16:00 +0600

    A simple Introduction to State change in React

--- react-core-concept/explore-core-react-part2/src/App.jsx
+++ react-core-concept/explore-core-react-part2/src/App.jsx
@@ -2,9 +2,38 @@ import './App.css'
 
 function App() {
 
+  function handleClick(){
+    alert('Clicked');
+  }
+  const handleClick3 = () => {
+    alert('Clicked 3');
+  }
+
+  const handleAdd5 = (num) => {
+    const newNum = num + 5;
+    alert(newNum);
+  }
+
   return (
     <>
       <h1>Vite + React</h1>
+
+      {/* <button onclick="handleClick()">Click Me</button> */}
+
+      <button onClick={handleClick}>Click Me</button> 
+
+      <button onClick={function handleClick2(){
+        alert('Clicked 2');
+      }}>Click Me 2</button>
+
+      <button onClick={handleClick3}>Click Me 3</button>
+
+      <button onClick={() => {
+        alert('Clicked 4');
+      }}>Click Me 4</button>
+
+      <button onClick={() => handleAdd5(7)}>Click Me 5</button>
+
     </>
   )
 }
