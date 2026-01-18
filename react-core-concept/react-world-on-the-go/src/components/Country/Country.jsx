8bb026c22d426d53f0f657c89c1b0a7e5d358358
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2026-01-19 01:35:13 +0600

    Conditional CSS and Toggle state based on click

--- react-core-concept/react-world-on-the-go/src/components/Country/Country.jsx
+++ react-core-concept/react-world-on-the-go/src/components/Country/Country.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
 
 const Country = ({country}) => {
     // console.log(country.flags);
     // console.log(country.fla);
 
+    const [visited, setVisited] = useState(false);
+
     const handleVisited = () => {
-        console.log('Visited Clicked!')
+        // console.log('Visited Clicked!')
+        // setVisited(true);
+
+        // if(visited === true){
+        //     setVisited(false);
+        // }else{
+        //     setVisited(true);
+        // }
+
+        setVisited(!visited);
     }
 
     return (
-        <div className="country">
+        // <div className="country">
+        <div className={`country ${visited && 'country-visited'}`}>
             <h3>Name: {country.name.common}</h3>
             {/* <img src={country.flags.pn}></img> */}  {/* --- Wrong property (No Error) --- */}
             {/* <img src={country.fla?.pn}></img> */}   {/* --- Wrong property (But Working) --- */}
@@ -19,7 +31,7 @@ const Country = ({country}) => {
             <img src={country.flags.png}></img>
             <p>Independent: {country.independent ? 'Free' : 'Not Free'}</p>
             <p>Population: {country.population}</p>
-            <button onClick={handleVisited}>Not Visited</button>
+            <button className={visited ? 'btn-visited' : 'btn-not-visited'} onClick={handleVisited}>{visited ? 'Visited' : 'Not Visited'}</button>
             <br></br>
         </div>
     );
