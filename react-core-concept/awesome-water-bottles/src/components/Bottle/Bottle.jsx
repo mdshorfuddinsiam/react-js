20fa2a1d8717925c4f60632c3b81b312c10d41d2
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2026-01-21 02:04:42 +0600

    (Recap) Load Bottles Data and show in grid layout

--- react-core-concept/awesome-water-bottles/src/components/Bottle/Bottle.jsx
+++ react-core-concept/awesome-water-bottles/src/components/Bottle/Bottle.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Bottle.css';
 
-const Bottle = ({bottle}) => {
+const Bottle = ({bottle, handleAddToCart}) => {
 
-    console.log(bottle);
+    // console.log(bottle);
+    const {img, name, price, stock} = bottle;
 
     return (
-        <div className='card'>
-            {
-                <img src={bottle.img}></img>
-            }
+        <div className='card bottle'>
+            <img src={img}></img>
+            <h3>{name}</h3>
+            <p>${price}</p>
+            <p>{stock} remaining</p>
+            <button onClick={() => handleAddToCart(bottle)}>Buy Now</button>
         </div>
     );
-};
+}
 
 export default Bottle;
--- react-core-concept/awesome-water-bottles/src/components/Bottles/Bottles.jsx
+++ react-core-concept/awesome-water-bottles/src/components/Bottles/Bottles.jsx
@@ -1,17 +1,28 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Bottle from '../Bottle/Bottle';
+import './Bottles.css';
 
 const Bottles = ({bottlesPromise}) => {
+    const [cart, setCart] = useState([]);
 
     const bottles = use(bottlesPromise);
     // console.log(bottles);   
 
+    const handleAddToCart = (bottle) => {
+        console.log('Bottle will be added to the cart', bottle);
+    }
+
     return (
         <div>
             <h3>Bottles: {bottles.length}</h3>
+            <div className="bottles-container">
             {
-                bottles.map(bottle => <Bottle bottle={bottle}></Bottle>)
+                bottles.map(bottle => <Bottle 
+                    key={bottle.id} 
+                    handleAddToCart={handleAddToCart}
+                    bottle={bottle}></Bottle>)
             }
+            </div>
         </div>
     );
 };
