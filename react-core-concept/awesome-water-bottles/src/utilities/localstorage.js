a530f9762ee4148faae5943cb6d0597ae2850f42
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2026-01-21 21:43:09 +0600

    [optional] Introduction to Local Storage: Storing Data in the Browser

---
+++ react-core-concept/awesome-water-bottles/src/utilities/localstorage.js
@@ -0,0 +1,25 @@
+const getCartFromLocalStorage = () =>{
+    const storedCartString = localStorage.getItem('cart');
+    
+    if(storedCartString){
+        const storedCart = JSON.parse(storedCartString);
+        return storedCart;
+    }
+    return [];
+}
+
+const saveCartToLocalStorage = cart => {
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem(cartStringified);
+}
+
+const addItemToCartLocalStorage = (id) => {
+    const cart = getCartFromLocalStorage();
+    // cart.push(id);      // working
+    const newCart = [...cart, id];
+
+    // save new cart to local storage
+    saveCartToLocalStorage(id);
+}
+
+export {getCartFromLocalStorage, addItemToCartLocalStorage}
