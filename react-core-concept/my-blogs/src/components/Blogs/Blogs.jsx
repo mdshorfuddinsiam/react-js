223b92ff24af5681839aafdbbfd03a001c653af3
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2026-01-23 17:20:13 +0600

    correction

--- react-core-concept/my-blogs/src/components/Blogs/Blogs.jsx
+++ react-core-concept/my-blogs/src/components/Blogs/Blogs.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-// const Blogs = (props) => {
-//     console.log(props);
-const Blogs = ({blogs}) => {
-    // console.log(blogs);
+const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch('blogs.json')
+        .then(res => res.json())
+        // .then(data => console.log(data))
+        .then(data => setBlogs(data))
+    }, []);
+
+    console.log(blogs);
 
     return (
         <div>
