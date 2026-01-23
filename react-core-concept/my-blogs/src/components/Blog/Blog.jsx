156923a86a0180406abae8afabfb56869b4ed777
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2026-01-23 19:11:16 +0600

    Add Bookmark button

--- react-core-concept/my-blogs/src/components/Blog/Blog.jsx
+++ react-core-concept/my-blogs/src/components/Blog/Blog.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaBookmark } from "react-icons/fa";
 
 // const Blog = (props) => {
 //     // console.log(props);
@@ -7,16 +8,27 @@ import React from 'react';
 //     console.log(blog);
 const Blog = ({ blog }) => {
     console.log(blog);
+    // console.log(blog.hashtags);
 
     return (
         <div>
             <div className="card bg-base-100 w-96 shadow-sm">
                 <figure>
-                    <img
-                        src={ blog.cover } />
+                    <img src={ blog.cover } />
                 </figure>
                 <div className="card-body">
+                    <div className="author flex justify-around items-center">
+                        <h3>{blog.author}</h3>
+                        <img className='w-16' src={blog.author_img} alt="" />
+                        <FaBookmark size={25} />
+                    </div>
                     <h2 className="card-title">{ blog.title }</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="flex">
+                        {
+                            blog.hashtags.map(hashtag => <p>{hashtag}</p>)
+                        }
+                    </div>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Mark as read</button>
                     </div>
