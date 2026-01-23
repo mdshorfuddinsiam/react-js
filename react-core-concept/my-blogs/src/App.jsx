29016d88dade7b4df75d6d32141b91c46c0c2ee8
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2026-01-23 17:08:22 +0600

    Create Fake Data for the blogs and load data

--- react-core-concept/my-blogs/src/App.jsx
+++ react-core-concept/my-blogs/src/App.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar/Navbar';
+import Blogs from './components/Blogs/Blogs';
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  useState(() => {
+    fetch('blogs.json')
+      .then(res => res.json())
+      // .then(data => console.log(data))
+      .then(data => setBlogs(data))
+  }, []);
 
   return (
     <>
       <Navbar></Navbar>
+      <Blogs blogs={blogs}></Blogs>
     </>
   )
 }
---
+++ react-core-concept/my-blogs/src/components/Blogs/Blogs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// const Blogs = (props) => {
+//     console.log(props);
+const Blogs = ({blogs}) => {
+    // console.log(blogs);
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
